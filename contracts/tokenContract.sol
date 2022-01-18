467b96472b7e0e2aae79c5b782e14e30b1b7e9ea
Abhinav Srivastava <atg271@gmail.com>
2022-01-18 14:04:47 -0800

    Update tokenContract.sol

--- contracts/tokenContract.sol
+++ contracts/tokenContract.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.2;
 
 import "../contracts/ERC20FeeAndBurn.sol";
-// import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract HYDROGENX is ERC20FAB {
     constructor() ERC20FAB("HYDROGENX", "HDNX", 0x1f4186d627ED0fD3532470Ac8d681D3FEC2fbA11, 2, 2) {
