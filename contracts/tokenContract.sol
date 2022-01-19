017075edb998fa7f5ec3ad07b3013203efdbe4d6
Abhinav Srivastava <atg271@gmail.com>
2022-01-19 09:56:50 -0800

    Update tokenContract.sol

--- contracts/tokenContract.sol
+++ contracts/tokenContract.sol
@@ -5,7 +5,7 @@ import "../contracts/ERC20FeeAndBurn.sol";
 // import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Example is ERC20FAB {
-    constructor() ERC20FAB("HYDROGENX", "HDNX", 0x1f4186d627ED0fD3532470Ac8d681D3FEC2fbA11, 2, 2) {
+    constructor() ERC20FAB("EXAMPLE", "EXMPL", 0x1f4186d627ED0fD3532470Ac8d681D3FEC2fbA11, 2, 2) {
         _mint(msg.sender, 100000 * 10 ** decimals());
     }
 }
